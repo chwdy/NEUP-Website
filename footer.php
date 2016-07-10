7def8afeb561cbfff5c8c39d2790f56b1dc4e0bf
Chwdy <1059126322@qq.com>
2016-07-10 11:15:03 +0800

    modify footer

--- footer.php
+++ footer.php
@@ -26,7 +26,7 @@
     <nav class="footer-info" id="footer_left">
 
         <p class="text">版权所有: 共青团东北大学委员会  |  地址：辽宁省沈阳市和平区文化路三巷11号  |  邮编：110819  |  辽ICP备05001360号  |  Northeastern University 2016  |  设计制作：<a href="#" target="_blank">团委</a>，技术运维：<a href="http://network.neu.edu.cn" target="_blank">网络中心</a> </p>
-        <p class =" text">今日访问量: <?php echo wp_statistics_visitor("today")?> 次 | 本周访问量: <?php echo wp_statistics_visitor("week") ?>次 | 本站总访问量: <?php echo wp_statistics_countusers() ?> 次</p>
+        <p class ="text">今日访问量: <?php echo wp_statistics_visitor("today")?> 次 | 本周访问量: <?php echo wp_statistics_visitor("week") ?>次 | 本站总访问量: <?php echo wp_statistics_countusers() ?> 次</p>
 
     </nav>
 
@@ -36,7 +36,9 @@
         <ul class="content">
             <li class="img"></li>
             <li class="text">东北大学团委官方微信<br>neutuanwei123</li>
-            <li class="wechat-qrcode"></li>
+
+           <!-- <li class="wechat-qrcode"></li>-->
+
         </ul>
     </a>
     <a class="weibo" href="#" target="_blank">
